ce9990cedbed49c14551cef6a5024e97f5155857
olszewskimichal <michalolsz@mat.umk.pl>
2016-04-15 16:39:32 +0200

    test

--- README.md
+++
@@ -1,2 +0,0 @@
-# Aluminium
-28-11-2015 wersja 0.0.1
--- src/main/java/com/zespolowka/repository/CustomNotificationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.zespolowka.repository;
-
-
-import com.zespolowka.entity.user.Role;
-
-public interface CustomNotificationRepository {
-    Long messageCount(long userId, Role role);
-}
