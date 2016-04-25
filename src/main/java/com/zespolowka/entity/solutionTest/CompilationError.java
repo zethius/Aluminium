af231ae8f862d9c253a54fc67a04d666f05ee879
peps <zethius@mat.umk.pl>
2016-04-25 17:02:11 +0200

    Aktywne zakladki, footer, Przekierowanie do /login dla niezalogowanego, usuniecie MainPage z navbar, przestawienie /sendMessage do wiadomosci i udostepnienie go dla usera,

---
+++ src/main/java/com/zespolowka/entity/solutionTest/CompilationError.java
@@ -0,0 +1,50 @@
+package com.zespolowka.entity.solutionTest;
+
+import javax.persistence.*;
+
+@Entity
+public class CompilationError {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    private CompilationErrorTypes type;
+    private String Error;
+
+
+    public CompilationError() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public CompilationErrorTypes getType() {
+        return type;
+    }
+
+    public void setType(CompilationErrorTypes type) {
+        this.type = type;
+    }
+
+    public String getError() {
+        return Error;
+    }
+
+    public void setError(String error) {
+        Error = error;
+    }
+
+    @Override
+    public String toString() {
+        return "CompilationError{" +
+                "id=" + id +
+                ", type='" + type + '\'' +
+                ", Error='" + Error + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/zespolowka/entity/solutionTest/CompilationErrorTypes.java
@@ -0,0 +1,23 @@
+package com.zespolowka.entity.solutionTest;
+
+
+public enum CompilationErrorTypes {
+    TERMINATED("Terminated"),
+    ERROR("Error"),
+    RESTRICTED_WORDS("Restricted words"),
+    COMPILATION_ERROR("Compilation Error");
+
+    private String value;
+
+    CompilationErrorTypes(String displayName) {
+        this.value=displayName;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
